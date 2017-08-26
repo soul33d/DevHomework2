d48d96f0e1db832925375c99f67be41f75f89cd4
Vitaliy Ostapenko <soul33d@gmail.com>
2017-08-26 17:06:29 +0300

    added basic model classes

--- src/main/java/model/Developer.java
+++ src/main/java/model/Developer.java
@@ -1,4 +1,42 @@
 package model;
 
+import java.math.BigDecimal;
+
 public class Developer {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private BigDecimal salary;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public BigDecimal getSalary() {
+        return salary;
+    }
+
+    public void setSalary(BigDecimal salary) {
+        this.salary = salary;
+    }
 }
--- src/main/java/model/Project.java
+++ src/main/java/model/Project.java
@@ -1,4 +1,33 @@
 package model;
 
+import java.math.BigDecimal;
+
 public class Project {
+    private int id;
+    private String name;
+    private BigDecimal cost;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getCost() {
+        return cost;
+    }
+
+    public void setCost(BigDecimal cost) {
+        this.cost = cost;
+    }
 }
